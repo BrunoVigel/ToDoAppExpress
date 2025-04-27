f120e692d1d3f9207228c314c22c5afa864fde37
Bruno <brunorosav@gmail.com>
2025-04-27 09:40:50 -0300

    função de delete

--- public/script.js
+++ public/script.js
@@ -1,11 +1,23 @@
 document.addEventListener('click', async function(e){
+    //delete
+    if(e.target.classList.contains('delete-me')){
+        if(confirm('Do you really want to delete?')){
+            axios.post('/delete-item', {id: e.target.getAttribute('data-id')}).then(function(){
+                e.target.parentElement.parentElement.remove()
+            }).catch(function(){
+                console.log('Please try again later')
+            })
+        }
+    }
+
+    //update
     if(e.target.classList.contains('edit-me')){
         let userInput = prompt('Enter your desired new text', e.target.parentElement.parentElement.querySelector('.item-text').innerHTML)
         if(userInput){
             axios.post('/update-item', {text: userInput, id: e.target.getAttribute('data-id')}).then(function(){
                 e.target.parentElement.parentElement.querySelector('.item-text').innerHTML = userInput
-            }).catch(function(){
-                console.log('Please try again later')
+            }).catch(function(e){
+                console.log(e)
             })
         }
     }
